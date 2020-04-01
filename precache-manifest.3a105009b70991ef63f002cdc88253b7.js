20ee11ab8c78455a5fde2d768e7b60a752036b4e
Tendai Chikake <tendaichikae@gmail.com>
2020-04-01 14:02:55 +0300

    Updates

---
+++ precache-manifest.3a105009b70991ef63f002cdc88253b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08caf2230f7de91df7d348ed0c8c39f4",
+    "url": "/eye-tracker/index.html"
+  },
+  {
+    "revision": "517037ad7fe752745e2c",
+    "url": "/eye-tracker/static/css/main.b9ab9fab.chunk.css"
+  },
+  {
+    "revision": "64f4b780f79bedaa52f1",
+    "url": "/eye-tracker/static/js/2.ab668fe7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eye-tracker/static/js/2.ab668fe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517037ad7fe752745e2c",
+    "url": "/eye-tracker/static/js/main.8870581c.chunk.js"
+  },
+  {
+    "revision": "c3dec18d30a7dc5b925e",
+    "url": "/eye-tracker/static/js/runtime-main.fb65618c.js"
+  }
+]);
