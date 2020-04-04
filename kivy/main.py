be0605013f63284ffb31bca9ef5d4e0514f81c9f
Tendai Chikake <tendaichikae@gmail.com>
2020-04-04 14:30:38 +0300

    add Tobii research, localisation and logging

--- kivy/main.py
+++ kivy/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.factory import Factory
@@ -7,36 +8,49 @@ from kivy.uix.popup import Popup
 from kivy.clock import Clock
 
 import os
-import tobiiresearch as tr
+import json
+import tobii_research as tr
 
 from kivy.core.window import Window
 import time
 
 Window.clearcolor = (1, 1, 1, 1)
 
+ERROR = 1
+WARNING = 2
+INFO = 3
+LOCALE = {}
+with open("_locale.json", "r") as f:
+    LOCALE = json.load(f)
+
+LOCALE["__empty"] = {
+    "ru" : "",
+    "en" : ""
+}
+
 def props(cls):   
   return [i for i in cls.__dict__.keys() if i[:1] != '_']
+
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
+    get_local_str = ObjectProperty(None)
 
 
-class Root(FloatLayout):
-    loadfile = ObjectProperty(None)
-    text_input = ObjectProperty(None)
-
+class Root(FloatLayout):        
     def init_tracker(self):
         if 'eyetracker' not in props(self):
-            # found_eyetrackers = tr.find_all_eyetrackers()
-            # self.eyetracker = found_eyetrackers[0]
-            # self.eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, self.capture, as_dictionary=True)
-            self.eyetracker = "tracker 1"
-            self.capture_up = True
-            fps = 0.3
-            Clock.schedule_interval(self.capture, 1.0/fps)
-       
-        print("done building here ")
+            found_eyetrackers = tr.find_all_eyetrackers()
+            if found_eyetrackers:
+                self.eyetracker = found_eyetrackers[0]
+                self.eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, self.capture, as_dictionary=True)
+            
+                self.capture_up = True
+                fps = 0.3
+                # Clock.schedule_interval(self.capture, 1.0/fps)
+            else:
+                self.applog(self.get_local_str("_tobii_not_found"), WARNING)
 
     def save_dir_ready(self):
         lbl_output_dir = self.ids['lbl_output_dir']
@@ -44,75 +58,104 @@ class Root(FloatLayout):
         ready = 'save_path' in props(self)
         if not ready:
             lbl_output_dir.color = (1,0,0,1)
+            self.applog(self.get_local_str("_directory_not_selected"))
         else:
             lbl_output_dir.color = (0,0,0,1)
 
         return ready
+    
+    def eyetracker_ready(self):
+        ready = 'eyetracker' in props(self)
+
+        error_log = self.get_local_str("_tobii_not_found")
+
+        if not ready:
+            self.applog(error_log, ERROR)
+        
+        return ready
+
+    def applog(self, text, logtype = INFO):
+        str_logtype = {
+            INFO : "INFO",
+            ERROR : "ERROR",
+            WARNING: "WARNING"
+        }
+        timestr = time.strftime("%Y/%m/%d %H:%M:%S")
+        app_log = self.ids['app_log']
+        log = "{}\t {} \n {} \n".format(str_logtype.get(logtype, INFO), timestr, text)
+
+        app_log.text = log + app_log.text
 
     def btn_play_click(self):
         camera = self.ids['camera']
         toggle_play = self.ids['toggle_play']
-        
+    
         if not self.save_dir_ready():
+            toggle_play.state = 'normal'
             return 
-        
+        if not self.eyetracker_ready():
+            toggle_play.state = 'normal'
+            return
         if camera.play:
             # next click will subscribe to tracker 
-            toggle_play.text = 'Play'
+            toggle_play.text = self.get_local_str('_start')
             self.capture_up = False
-            # eyetracker.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, gaze_data_callback)
-            print("next click will be subscribing")
+            self.eyetracker.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, self.capture)
         else:
             # will be subscribing to tracker
-            toggle_play.text = 'Stop'
+            toggle_play.text = self.get_local_str('_stop')
             self.capture_up = True
-            # eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dictionary=True)
-            print("next click will be unsubscribing")
+            self.eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, self.capture, as_dictionary=True)
             
         camera.play = not camera.play
 
     def capture(self,gaze_data):
         '''
-        Function to capture the images and give them the names
-        according to their captured time and date.
+        Callback Function to capture the images with the respective gaze record and give them the names
+        according to their captured time and date and record all to log.
         '''
-
-        #  simulation using clock 
-        gaze_data = {
-            'left_gaze_point_on_display_area': 24,
-            'right_gaze_point_on_display_area': 200,
-        }
 
         if not self.save_dir_ready():
             return
 
-        if 'capture_up' in props(self):            
-            if not self.capture_up:
-                return 
+       
+        timestr = time.strftime("%Y%m%d_%H%M%S")
+        filename = "IMG_{}.png".format(timestr)
 
-            timestr = time.strftime("%Y%m%d_%H%M%S")
-            filename = "IMG_{}.png".format(timestr)
+        camera = self.ids['camera']
+        camera.export_to_png(os.path.join(self.save_path,filename))
 
-            camera = self.ids['camera']
-            camera.export_to_png(os.path.join(self.save_path,filename))
+        timestr = time.strftime("%Y/%m/%d %H:%M:%S") # for log format as user friendly date 
 
-            record = "{timestr}, {gaze_left_eye}, {gaze_right_eye}, {frame} \n".format(
-                timestr=timestr,
-                gaze_left_eye=gaze_data['left_gaze_point_on_display_area'],
-                gaze_right_eye=gaze_data['right_gaze_point_on_display_area'],
-                frame=filename)
-            
-            gaze_log = self.ids['gaze_log']
-            gaze_log.text = gaze_log.text + record
+        record = "{timestr}, {gaze_left_eye}, {gaze_right_eye}, {frame} \n".format(
+            timestr=timestr,
+            gaze_left_eye=gaze_data['left_gaze_point_on_display_area'],
+            gaze_right_eye=gaze_data['right_gaze_point_on_display_area'],
+            frame=filename)
+        
+        # write log to file
+        with open(os.path.join(self.save_path,"log.csv"), "a") as f:
+            f.write(record)
+            f.close()
+
+        gaze_log = self.ids['gaze_log']
+        gaze_log.text = record + gaze_log.text
 
     def dismiss_popup(self):
         self._popup.dismiss()
 
     def show_load(self):
-        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Select directory", content=content,
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup, get_local_str = self.get_local_str)
+        self._popup = Popup(title=self.get_local_str("_select_directory"), content=content,
                             size_hint=(0.9, 0.9))
         self._popup.open()
+
+    def get_local_str(self, key):
+        lang = "ru"
+        if key in LOCALE:
+            return LOCALE.get(key)[lang]
+
+        return LOCALE["__empty"][lang]
 
     def load(self, path, filename):
         self.save_path = path
