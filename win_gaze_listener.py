400fe6194921a6b6b8ffbe6534e37632f486538f
Tenfleques <tendaichikake@gmail.com>
2020-04-21 14:09:41 +0300

    dll captures interactive

--- win_gaze_listener.py
+++ win_gaze_listener.py
@@ -1,21 +1,33 @@
 import os
 import sys
-from ctypes import cdll, c_double, c_float
+from ctypes import cdll, c_double, c_bool, c_int, Structure, POINTER
 from numpy.ctypeslib import ndpointer
 from helpers import props
 import time
+import numpy as np
+
+
+class Record(Structure):
+    _fields_ = [('x', c_double),
+            ('y', c_double),
+            ('timestamp', c_double),
+            ('engineTimestamp', c_double),
+            ('valid', c_bool)
+        ]
 
 path = "C:\\Users\\tendai\\source\\repos\\TobiiEyeLib\\x64\\Debug\\TobiiEyeLib.dll"
 lib = cdll.LoadLibrary(path)
 
-lib.getLatest.restype = ndpointer(dtype=c_double, shape=(10, 4))
+lib.stop.restype = c_int
+
+lib.getLatest.restype = POINTER(Record)
 
 print(lib.start())
 i = 0
 while i < 4:
     output = lib.getLatest()
-    print(output[0])
-    time.sleep(5)
+    print(output[0].x, output[0].y, output[0].valid, output[0].timestamp)
+    time.sleep(1)
     i += 1
 
 print(lib.stop())
